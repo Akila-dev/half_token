ebaefa8c88bdb0e3041f23771261b5d950235db1
unknown <akilapanunayan@gmail.com>
2024-02-26 13:06:37 +0100

    stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -20,21 +20,23 @@ const Stars = (props) => {
 		console.log(sphere);
 	}, []);
 
-	useFrame((state, delta) => {
-		ref.current.rotation.x -= delta / 10;
-		ref.current.rotation.y -= delta / 15;
-	});
+	// useFrame((state, delta) => {
+	// 	ref.current.rotation.x -= delta / 10;
+	// 	ref.current.rotation.y -= delta / 15;
+	// });
 
 	return (
-		<Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
-			<PointMaterial
-				transparent
-				color="#f272c8"
-				size={0.002}
-				sizeAttenuation={true}
-				depthWrite={false}
-			/>
-		</Points>
+		<group rotation={[0, 0, Math.PI / 4]}>
+			<Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+				<PointMaterial
+					transparent
+					color="#f272c8"
+					size={0.002}
+					sizeAttenuation={true}
+					depthWrite={false}
+				/>
+			</Points>
+		</group>
 	);
 };
 
